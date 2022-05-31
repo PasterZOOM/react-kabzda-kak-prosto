6a69e23d04ca78cd89311e163e736af6c1b98c7b
Ilya Pasternak <pasterzoom@gmail.com>
2022-05-31 21:03:44 +0300

    22 - useEffect \ setTimeout \ setInterval

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -11,15 +11,15 @@ export const Example = () => {
 
     console.log('SimpleExample')
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log('useEffect every render')
         document.title = counter.toString()
     })
-    useEffect(()=>{
+    useEffect(() => {
         console.log('useEffect only first render (componentDidMount)')
         document.title = counter.toString()
     }, [])
-    useEffect(()=>{
+    useEffect(() => {
         console.log('useEffect first render and every counter change')
         document.title = counter.toString()
     }, [counter])
@@ -29,4 +29,44 @@ export const Example = () => {
         <button onClick={() => setFake(fake + 1)}>fake +</button>
         <button onClick={() => setCounter(counter + 1)}>counter +</button>
     </>
-}  
+}
+
+export const SetTimeoutExample = () => {
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1)
+
+    console.log('SetTimeoutExample')
+
+    useEffect(() => {
+
+        setInterval(() => {
+            console.log('tick' + counter)
+            setCounter(state => state + 1)
+        }, 1000)
+
+    }, [])
+
+    return <>
+        Hello, counter: {counter} fake: {fake}
+        {/*<button onClick={() => setFake(fake + 1)}>fake +</button>*/}
+        {/*<button onClick={() => setCounter(counter + 1)}>counter +</button>*/}
+    </>
+}
+
+export const TimeExample = () => {
+
+    const [date, setDate] = useState(new Date())
+    console.log('SetTimeoutExample')
+
+    useEffect(() => {
+
+        setInterval(() => {
+            setDate(() => new Date())
+        }, 1000)
+
+    }, [])
+
+    return <>
+        {date.getHours()}:{date.getMinutes()}:{date.getSeconds()}
+    </>
+}
